5cc5117862274457032791abc0e2f4ef31914da6
derlin-asp <daviddstacey@gmail.com>
2022-01-15 14:13:07 -0800

    minor fixxes

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#Good morning! Here's your coding interview problem for today.
+#
+# This problem was recently asked by Google.
+#
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+#
+# Upgrade to premium and get in-depth solutions to every problem, including this one.
+#
+# If you liked this problem, feel free to forward it along so they can subscribe here! As always, shoot us an email if there's anything we can help with!
+#
+# Master algorithms together on Binary Search. Create a room, invite your friends, and race to finish the problem!
+
 
 def twoSumsBooleanFast():
     #cannnot use position
@@ -8,7 +24,7 @@ def twoSumsBooleanFast():
     k = 17
     my_dict = {}
     # time complexly = n
-    # space = N^2
+    # space = 2N = n
     for x in range(len(nums)):
         my_dict[nums[x]] = True
         diff = k - x
@@ -26,7 +42,7 @@ def twoSumsBooleanBrute():
     nums = [1,7,2,4,5,6,3,8,9]
     k = 2
     if len(nums) == 0:
-        return
+        return False
 
     for x in range(len(nums)):
         for y in range(len(nums)):
